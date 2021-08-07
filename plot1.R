357fc88713b0e90813dec83fe833f4105cbb145b
geeccy <yu_erwin@hotmail.com>
2021-08-07 15:55:51 +1000

    initial commit

---
+++ plot1.R
@@ -0,0 +1,36 @@
+#Using the tidyverse libraries
+library(tidyverse)
+library(lubridate)
+
+#Extract whole dataset from text file
+data <- read.csv("household_power_consumption.txt", sep=";")
+
+#create data slice for analysis
+dataSample <- data %>% 
+  mutate(Date = dmy(Date)) %>% 
+  mutate(Time = hms(Time)) %>% 
+  mutate(Global_active_power = as.numeric(Global_active_power)) %>% 
+  mutate(Global_reactive_power  = as.numeric(Global_reactive_power)) %>% 
+  mutate(Voltage  = as.numeric(Voltage)) %>% 
+  mutate(Global_intensity = as.numeric(Global_intensity)) %>% 
+  mutate(Sub_metering_1 = as.numeric(Sub_metering_1)) %>% 
+  mutate(Sub_metering_2 = as.numeric(Sub_metering_2)) 
+
+#Take only required dates  
+dataSample <- filter(dataSample, (Date == dmy("01/02/2007")|Date==ymd("2007-02-02"))) 
+
+#Remove NA values
+dataSample <- dataSample[complete.cases(dataSample),]
+
+
+#output to png
+png("plot1.png")
+
+hist(dataSample$Global_active_power, 
+     col="red", 
+     xlab = "Global Active Power (kilowatts)",
+     main="Global Active Power")
+
+
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,45 @@
+#Using the tidyverse libraries
+library(tidyverse)
+library(lubridate)
+
+#Extract whole dataset from text file
+data <- read.csv("household_power_consumption.txt", sep=";")
+
+#create data slice for analysis
+dataSample <- data %>% 
+  mutate(Date = dmy(Date)) %>% 
+  mutate(Time = hms(Time)) %>% 
+  mutate(Global_active_power = as.numeric(Global_active_power)) %>% 
+  mutate(Global_reactive_power  = as.numeric(Global_reactive_power)) %>% 
+  mutate(Voltage  = as.numeric(Voltage)) %>% 
+  mutate(Global_intensity = as.numeric(Global_intensity)) %>% 
+  mutate(Sub_metering_1 = as.numeric(Sub_metering_1)) %>% 
+  mutate(Sub_metering_2 = as.numeric(Sub_metering_2)) 
+
+#Take only required dates  
+dataSample <- filter(dataSample, (Date == dmy("01/02/2007")|Date==ymd("2007-02-02"))) 
+dataSample <- mutate(dataSample, DateTime = Date + Time)
+
+#Remove NA values
+dataSample <- dataSample[complete.cases(dataSample),]
+
+
+#output to png
+png("plot3.png")
+
+#Set up plot
+plot(dataSample$Sub_metering_1 ~ dataSample$DateTime, type="n", xlab="", ylab="Energy Sub metering")
+
+#Add lines
+lines(dataSample$Sub_metering_1 ~ dataSample$DateTime)
+lines(dataSample$Sub_metering_2 ~ dataSample$DateTime, col="red")
+lines(dataSample$Sub_metering_3 ~ dataSample$DateTime, col="blue")
+
+#Add legend
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"),
+       lwd = 2)
+
+dev.off()
+
